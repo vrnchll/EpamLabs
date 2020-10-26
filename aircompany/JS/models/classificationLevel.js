0068554f00da9920a29a480f056bd8b042a82bea
vrnchll <54811120+vrnchll@users.noreply.github.com>
2020-10-26 18:56:00 +0300

    lab6

---
+++ aircompany/JS/models/classificationLevel.js
@@ -0,0 +1,8 @@
+const ClassificationLevel  = {
+    UNCLASSIFIED: 'UNCLASSIFIED',
+    CONFIDENTIAL: 'CONFIDENTIAL',
+    SECRET: 'SECRET',
+    TOP_SECRET: 'TOP_SECRET'
+};
+
+module.exports =  ClassificationLevel;
